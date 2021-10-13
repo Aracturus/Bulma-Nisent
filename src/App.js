501e71ed9d83d3209ef71b675b2bf4f0d027272c
Aracturus <iconwebdev1@gmail.com>
2021-10-13 22:42:36 +0530

    done

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import logo from './logo.svg';
+import './App.css';
+import Navbar from './Components/Navbar';
+import ConpOne from './Components/ConpOne';
+import CompTwo from './Components/CompTwo';
+import CompThree from './Components/CompThree';
+import CompFour from './Components/CompFour';
+import Accordian from './Components/Accordian';
+import CompFive from './Components/CompFive';
+
+function App() {
+  return (
+    <>
+    <Navbar/>
+    <ConpOne/>
+    <CompTwo />
+    <CompThree/>
+    <CompFour/>
+    <Accordian/>
+    <CompFive />
+
+      
+    </>
+  );
+}
+
+export default App;
